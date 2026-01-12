df034ba5b103168aba2c71a260e602bc52a4e351
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-13 00:53:54 +0200

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,1 +1,27 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navStyle = {
+    display: "flex",
+    gap: "12px",
+    padding: "14px",
+    backgroundColor: "#111",
+  };
+
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
+
+  return (
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
 
