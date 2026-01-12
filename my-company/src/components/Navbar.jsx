5486206c83b5d879bed3333aa7fcac0a25b66d1d
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-13 01:49:00 +0200

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,24 +4,34 @@ function Navbar() {
   const navStyle = {
     display: "flex",
     gap: "12px",
-    padding: "14px",
+    padding: "14px 20px",
     backgroundColor: "#111",
   };
 
   const linkStyle = {
     color: "white",
     textDecoration: "none",
+    padding: "6px 10px",
+    borderRadius: "6px",
+    backgroundColor: "#222",
   };
 
   return (
     <nav style={navStyle}>
-      <Link to="/" style={linkStyle}>Home</Link>
-      <Link to="/about" style={linkStyle}>About</Link>
-      <Link to="/services" style={linkStyle}>Services</Link>
-      <Link to="/contact" style={linkStyle}>Contact</Link>
+      <Link to="/" style={linkStyle}>
+        Home
+      </Link>
+      <Link to="/about" style={linkStyle}>
+        About
+      </Link>
+      <Link to="/services" style={linkStyle}>
+        Services
+      </Link>
+      <Link to="/contact" style={linkStyle}>
+        Contact
+      </Link>
     </nav>
   );
 }
 
 export default Navbar;
-
