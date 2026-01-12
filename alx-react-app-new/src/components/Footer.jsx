524c5092d24de74266cf5a304f46ad7136106e26
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-13 01:24:26 +0200

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,1 +1,10 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "#111", color: "white", textAlign: "center", padding: "10px" }}>
+      <p style={{ margin: 0 }}>© 2026 My React App</p>
+    </footer>
+  );
+}
+
+export default Footer;
 
