bc2172b0949e46efb22b3ca32625a0c92dd9cebe
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-12 22:00:56 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from '../Header';
+import MainContent from '../MainContent';
+import Footer from '../Footer';
 
 function App() {
   return (
