bc6a140185962fb0625f0b219b5c17d0bded3ef1
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 16:44:56 +0200

    Create Blog.jsx

---
+++ react-router-advanced/src/pages/Blog.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+export default function Blog() {
+  const posts = [
+    { id: "1", title: "First Post" },
+    { id: "2", title: "Second Post" },
+    { id: "3", title: "Third Post" }
+  ];
+
+  return (
+    <div>
+      <h2>Blog</h2>
+      <p>Dynamic route: /blog/:postId</p>
+
+      <ul>
+        {posts.map((p) => (
+          <li key={p.id}>
+            <Link to={`/blog/${p.id}`}>{p.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
