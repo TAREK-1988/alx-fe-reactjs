eccd481ee09e258e9740eeb94b481625e40b8c33
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 16:47:21 +0200

    Create Login.jsx

---
+++ react-router-advanced/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login({ onLogin }) {
+  return (
+    <div>
+      <h2>Login</h2>
+      <p>This is a simulated login.</p>
+      <button onClick={onLogin}>Login</button>
+    </div>
+  );
+}
