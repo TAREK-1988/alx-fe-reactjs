5904ed61b1c23c3a07a62fc4448811461df31a21
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 21:14:48 +0200

    Update babel.config.cjs

--- react-todo/babel.config.cjs
+++ react-todo/babel.config.cjs
@@ -1,6 +1,8 @@
+cat > babel.config.cjs << 'EOF'
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     ["@babel/preset-react", { runtime: "automatic" }]
   ]
 };
+EOF
