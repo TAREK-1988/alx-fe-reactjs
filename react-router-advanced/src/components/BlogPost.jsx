f94fddf7fa79e5d5ef65fa6cadbe9b9f76a908d1
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 17:10:20 +0200

    Update BlogPost.jsx

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,12 +1,12 @@
 import { useParams, Link } from "react-router-dom";
 
 export default function BlogPost() {
-  const { postId } = useParams();
+  const { id } = useParams();
 
   return (
     <div>
       <h2>Blog Post</h2>
-      <p>Post ID: {postId}</p>
+      <p>Post ID: {id}</p>
       <Link to="/blog">Back to Blog</Link>
     </div>
   );
