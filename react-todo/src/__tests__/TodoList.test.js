e245f3febbe4ed6ce8f202aacda0998b7d0c7643
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 19:18:10 +0200

    Update TodoList.test.js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,10 +1,11 @@
+import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import TodoList from "../components/TodoList.jsx";
 
-describe("TodoList Component", () => {
+describe("TodoList", () => {
   test("renders initial todos", () => {
     render(<TodoList />);
-
     expect(screen.getByText("Learn React")).toBeInTheDocument();
     expect(screen.getByText("Write Tests")).toBeInTheDocument();
     expect(screen.getByText("Ship Project")).toBeInTheDocument();
@@ -14,19 +15,18 @@ describe("TodoList Component", () => {
     render(<TodoList />);
 
     const input = screen.getByLabelText("todo-input");
-    const addButton = screen.getByLabelText("add-todo-button");
-
     fireEvent.change(input, { target: { value: "New Todo Item" } });
-    fireEvent.click(addButton);
+
+    const button = screen.getByLabelText("add-todo-button");
+    fireEvent.click(button);
 
     expect(screen.getByText("New Todo Item")).toBeInTheDocument();
   });
 
-  test("toggles a todo when clicked", () => {
+  test("toggles todo completion", () => {
     render(<TodoList />);
 
     const todo = screen.getByLabelText("todo-Learn React");
-
     expect(todo).not.toHaveClass("completed");
 
     fireEvent.click(todo);
@@ -41,8 +41,8 @@ describe("TodoList Component", () => {
 
     expect(screen.getByText("Ship Project")).toBeInTheDocument();
 
-    const deleteBtn = screen.getByLabelText("delete-Ship Project");
-    fireEvent.click(deleteBtn);
+    const deleteButton = screen.getByLabelText("delete-Ship Project");
+    fireEvent.click(deleteButton);
 
     expect(screen.queryByText("Ship Project")).not.toBeInTheDocument();
   });
