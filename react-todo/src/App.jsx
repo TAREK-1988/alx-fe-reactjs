2d10e56d20d9d23e9913ffdf551019e93a223ad7
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 17:30:36 +0200

    Create App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import TodoList from "./components/TodoList.jsx";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Todo List</h1>
+        <TodoList />
+      </div>
+    </div>
+  );
+}
