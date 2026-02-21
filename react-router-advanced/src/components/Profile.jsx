0a6093aa4d0c4938884a7170f90cce0a66a1f80b
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 17:07:37 +0200

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails.jsx";
+import ProfileSettings from "./ProfileSettings.jsx";
 
 export default function Profile({ onLogout }) {
   return (
@@ -7,12 +9,17 @@ export default function Profile({ onLogout }) {
       <button onClick={onLogout}>Logout</button>
 
       <div style={{ marginTop: 12 }}>
-        <Link to="details" style={{ marginRight: 10 }}>Details</Link>
-        <Link to="settings">Settings</Link>
+        <Link to="/profile/details" style={{ marginRight: 10 }}>
+          Details
+        </Link>
+        <Link to="/profile/settings">Settings</Link>
       </div>
 
       <div style={{ marginTop: 16 }}>
-        <Outlet />
+        <Routes>
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Routes>
       </div>
     </div>
   );
