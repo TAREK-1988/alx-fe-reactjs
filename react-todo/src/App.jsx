806b0f642485b29e8a15d996607a4282fe6f3e06
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 21:19:50 +0200

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,12 @@
+cat > src/App.jsx << 'EOF'
 import TodoList from "./components/TodoList.jsx";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="card">
-        <h1>Todo List</h1>
-        <TodoList />
-      </div>
+    <div>
+      <h1>Todo List</h1>
+      <TodoList />
     </div>
   );
 }
+EOF
