cad529fbf8c6f99d3e3326ad4a6cc46f45d20786
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-21 17:02:09 +0200

    Update and rename Blog.jsx to  Blog.jsx

--- react-router-advanced/src/components/ Blog.jsx
+++ react-router-advanced/src/components/ Blog.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Blog() {
   const posts = [
-    { id: "1", title: "First Post" },
+    { id: "1", title: "First Post" },      
     { id: "2", title: "Second Post" },
     { id: "3", title: "Third Post" }
   ];
